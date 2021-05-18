cec6b3e90f5ce1d6165a46085158da2a1bc9087b
dibyajyoti <dibyajyotip@mindfiresolutions.com>
2021-05-18 22:11:53 +0530

    migrations created.

--- database/migrations/2021_05_18_161557_create_questions_table.php
+++ database/migrations/2021_05_18_161557_create_questions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateQuestionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('questions', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('picture')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->unsignedBigInteger('user_id');
+            $table->longText('body');
+            $table->string('image')->nullable();
             $table->mediumInteger('points')->default(0);
-            $table->rememberToken();
+            $table->mediumInteger('views')->default(0);
             $table->timestamps();
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users');
         });
     }
 
@@ -33,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('questions');
     }
 }
