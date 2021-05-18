955b0213bd22a880e0cf26db7dcaee7250da6472
dibyajyoti <dibyajyotip@mindfiresolutions.com>
2021-05-18 21:01:05 +0530

    Index page changes+Help button added

---
+++ resources/views/help.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Any Doubts?
+                </div>
+            </div>
+        </div>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,16 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+
+                <!-- If authenticated Logo will fetch home page else index page -->
+
+                @guest
+                <a class="navbar-brand" href="{{ route('index') }}">
+                @else
+                <a class="navbar-brand" href="{{ route('home') }}">
+                @endguest
+                    <!-- {{ config('app.name', 'Laravel') }} -->
+                    <span style="color: red;">Ask</span>me
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -48,6 +56,9 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('help') }}">Help</a>
+                            </li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -59,6 +70,9 @@
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('help') }}">
+                                        Help
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
